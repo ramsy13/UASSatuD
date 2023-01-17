a6fdd4c4a7cfb9c32036790c23fe0baad0f1885a
ramsyparker <ramad7287@gmail.com>
2023-01-17 12:22:18 +0700

    yes

---
+++ src/UAS1D.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+public class UAS1D {
+        private String MESSAGEID;
+        private String MESSAGENAME;
+        private ArrayList<String> CONTEXT ;
+
+        public String getMESSAGEID() {
+            return MESSAGEID;
+        }
+        public void setMESSAGEID(String MESSAGEID) {
+            this.MESSAGEID = MESSAGEID;
+        }
+        public String getMESSAGENAME() {
+            return MESSAGENAME;
+        }
+        public void setMESSAGENAME(String MESSAGENAME) {
+            this.MESSAGENAME = MESSAGENAME;
+        }
+        public ArrayList<String> getCONTEXT() {
+            return CONTEXT;
+        }
+        public void setCONTEXT(ArrayList<String> CONTEXT) {
+            this.CONTEXT = CONTEXT;
+        }
+        public void FindWord(ArrayList<String> list, String Cari ){
+            list.add(Cari);
+            for (int index = 0; index < list.size(); index++){
+                list.get(index).indexOf(Cari);
+            }
+            return "indeks ke "+list.indexOf(Cari);
+        }
+    }
